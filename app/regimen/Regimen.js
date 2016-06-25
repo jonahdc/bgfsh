6a80ba0028d7870269370d3e9c223d0c9ebfe6dd
fl <jonahlogy@gmail.com>
2016-06-26 00:37:24 +0800

    Add timeline structure

--- app/regimen/Regimen.js
+++ app/regimen/Regimen.js
@@ -20,13 +20,33 @@ export class Regimen extends React.Component {
           <div class="regimen-container">
             <div class="sidebar">
             <div class="widget">
-            <h3>New Observation</h3>
+            <h3 class="light-header">New Observation</h3>
             <form class="entry">
               <input type="text" name="title" class="entry__title" placeholder="Title" />
              <textarea name="desc" class="entry__desc" cols="40" rows="5" placeholder="Description"></textarea>
              <a href="javascript:void(0)" class="btn btn-primary" onClick={this.handlers.onSubmitClick}>Add</a>
               </form>
             </div>
+            </div>
+            <div class="body">
+              <div class="regimen-timeline">
+                <h3 class="light-header">Timeline</h3>
+                <div class="timeline-entries">
+                  <div class="timeline-entry">
+                  <div class="timeline-entry__meta">
+                    <span class="timeline-entry__datetime">4.30pm</span>
+                    </div>
+                    <div class="timeline-entry__content">
+                      <h3 class="timeline-entry__title">
+                      Title
+                      </h3>
+                      <div class="timeline-entry__desc">
+                      <p>This is a description.</p>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
             </div>
           </div>
           <div class="footer">
