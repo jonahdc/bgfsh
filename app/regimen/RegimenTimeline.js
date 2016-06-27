559b0cf7c15c3f483e9bd1504bdaf239d22bb914
fl <jonahlogy@gmail.com>
2016-06-27 13:46:14 +0800

    Modified layout and add animation to timeline

--- app/regimen/RegimenTimeline.js
+++ app/regimen/RegimenTimeline.js
@@ -2,6 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import { connect } from 'react-redux'
 import RegimenTimelineEntry from './RegimenTimelineEntry'
+import ReactCSSTransitionGroup from 'react-addons-css-transition-group'
 
 export default class RegimenTimeline extends React.Component {
 
@@ -19,7 +20,9 @@ render(){
     <div class="regimen-timeline">
       <h3 class="light-header">Timeline</h3>
       <div class="timeline-entries">
+        <ReactCSSTransitionGroup transitionName="timeline-animation">
       { timelineEntryList }
+    </ReactCSSTransitionGroup>
       </div>
     </div>
   );
